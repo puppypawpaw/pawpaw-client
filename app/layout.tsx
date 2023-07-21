c9e89a5bf195d6010a49cc5ca32c1051e7536b64
jiyeeeah <sije3757@snu.ac.kr>
2023-07-21 16:12:43 +0900

    Chore: 메인 페이지 폴더와 피드 파일 생성

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <body>
+<<<<<<< HEAD
         <Sidebar>
           <main className="p-8 m-auto ml-64">{children}</main>
         </Sidebar>
+=======
+        <div className="fixed left-0 w-64 h-screen bg-blue-300">d</div>
+        <main className="p-8 m-auto ml-64 ">
+          <div className="flex h-screen gap-10 border-2 ">
+            <div className="h-screen border-2 border-black"></div>
+            <div className="flex-col bg-white border-2 border-red-500 top-8 right-8">
+              <div>ddddddddddddd</div>
+            </div>
+          </div>
+          {children}
+        </main>
+>>>>>>> b5e7dcf (Chore: 메인 페이지 폴더와 피드 파일 생성)
       </body>
     </html>
   );
