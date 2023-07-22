d145940fbdcad43ea5c53d989b01dc193b1fa504
jiyeeeah <sije3757@snu.ac.kr>
2023-07-22 12:26:47 +0900

    Design: 메인 피드 레이아웃 설정 #2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import "../styles/globals.css";
+<<<<<<< HEAD
 import Sidebar from "@/components/ui/Sidebar/Sidebar";
+=======
+import MainPage from "@/components/pages/MainPage";
+>>>>>>> f8d1080 (Design: 메인 피드 레이아웃 설정 #2)
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,7 +17,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div className="fixed left-0 w-64 h-screen bg-blue-300">d</div>
         <main className="p-8 m-auto ml-64 ">
           <div className="flex h-screen gap-10 border-2 ">
-            <div className="h-screen border-2 border-black"></div>
+            <div className="h-screen border-2 border-black">
+              <MainPage></MainPage>
+            </div>
             <div className="flex-col bg-white border-2 border-red-500 top-8 right-8">
               <div>ddddddddddddd</div>
             </div>
