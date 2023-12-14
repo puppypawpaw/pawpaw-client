59e3c8cc3ed7a090f73782d17c051d413348ebb9
shseok <shseok0674@gmail.com>
2023-12-15 00:30:04 +0900

    Fix: pawzone search, place 페이지 메타데이터 추가

--- app/(main)/pawzone/place/[placeId]/page.tsx
+++ app/(main)/pawzone/place/[placeId]/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { Metadata } from 'next';
 import { getDetailedPlace } from '@/service/server/pawzone';
 import PlaceResult from '../../_components/Place/PlaceResult';
 
 interface Props {
   params: { placeId: string };
+}
+
+export async function generateMetadata({
+  params: { placeId },
+}: Props): Promise<Metadata> {
+  const {
+    name,
+    position: { address },
+    imageUrlList,
+  } = await getDetailedPlace(placeId);
+  return {
+    title: `${name} | Pawzone`,
+    description: address,
+    openGraph: {
+      images: { url: imageUrlList[0], alt: name },
+      title: name,
+      description: `${address}에 위치한 ${name} 입니다.`,
+      url: `/pawzone/place/${placeId}`,
+    },
+  };
 }
 
 // id로 이에 대한 데이터를 활용하기
--- app/(main)/pawzone/search/[result]/page.tsx
+++ app/(main)/pawzone/search/[result]/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { Metadata } from 'next';
 import SearchBar from '../../_components/Search/SearchBar';
 import SearchResult from '../../_components/Search/SearchResult';
 
 interface Props {
   params: { result: string };
+}
+
+export function generateMetadata({ params: { result } }: Props): Metadata {
+  const keyword = decodeURI(result);
+  return {
+    title: `${keyword} | Pawzone`,
+    description: `${keyword}에 대한 검색 결과입니다.`,
+  };
 }
 
 export default function PlacePage({ params: { result } }: Props) {
